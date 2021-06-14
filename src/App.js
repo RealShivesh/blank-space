50317ca40bc6f98139a4edd093ad53fa135a058a
RealShivesh <realshivesh@gmail.com>
2021-06-14 17:17:28 +0530

    Final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import "./App.css"
+import Logo from "./asset/google-logo.png"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    //Using useState to set the background color
+    const [color, setColor] = useState("#fff")
+    //txtHandler for performing the onChange function specified in the input box
+    const txtHandler = (e) => {
+        setColor(e.target.value)
+        console.log(e.target.value)
+    }
+
+    return (
+        <div
+            className="App"
+            style={{ backgroundColor: color, height: "100vh" }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            <div className="header" style={{ backgroundColor: "#fff" }}>
+                {" "}
+                {/*This is the header part, it won't change as per the textbox to provide text accesibility */}
+                Use the input box and see the magic ;)
+            </div>
+            <div className="page">
+                <img src={Logo} alt="" style={{ width: "30%" }} />
+                <input
+                    type="text"
+                    onChange={txtHandler}
+                    name="A dummy search"
+                    id=""
+                    placeholder="Type anything here"
+                    style={{ padding: "5px", width: "40%" }}
+                />
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
